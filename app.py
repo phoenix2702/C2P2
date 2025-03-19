f062478aac60a154389e4e043de394feb5d357ad
phoenix2702 <111878347+phoenix2702@users.noreply.github.com>
2025-03-19 19:21:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def index():
     return render_template("index.html", topics=all_topics, recommendation=recommendation)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Fixed: Added host and port
